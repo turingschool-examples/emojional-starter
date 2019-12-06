81ffde13e83716644fa77f6713cb7ea92947b938
Megan Venetianer <megan.venetianer@gmail.com>
2019-12-06 11:18:20 -0700

    Completed iteration 1

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var happyButton = document.querySelector('.happy');
 var happyMessage = ['You go', 'Yay!', 'Woohoo!'];
+var sillyButton = document.querySelector('.silly');
+var sillyMessage = ['Interesting response', 'Sounds...good?', 'Me too.'];
+var cryingButton = document.querySelector('.crying');
+var cryingMessage = ['Do you want to talk?', 'Keep your head up', 'We can cry together!', 'What can I do?'];
 
 happyButton.addEventListener('click', addHappyResponse);
+sillyButton.addEventListener('click', addSillyResponse);
+cryingButton.addEventListener('click', addCryingResponse);
 
 function addHappyResponse() {
   var randomMessage = Math.floor(Math.random() * (happyMessage.length));
   document.querySelector('.message').innerHTML = happyMessage[randomMessage];
+}
+
+function addSillyResponse() {
+  var randomMessage = Math.floor(Math.random() * (sillyMessage.length));
+  document.querySelector('.message').innerHTML = sillyMessage[randomMessage];
+}
+
+function addCryingResponse() {
+  var randomMessage = Math.floor(Math.random() * (cryingMessage.length));
+  document.querySelector('.message').innerHTML = cryingMessage[randomMessage];
 }
 
 // fixing cursor
