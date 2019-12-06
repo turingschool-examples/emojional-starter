cd888edeeea3e0308a9d053c10dc9ce979bbce9f
Jordan Whitten <jordanwa1947@gmail.com>
2019-12-06 09:29:31 -0700

    Attach event listener to happy emoji and test with console.log

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const happyEmoji = document.getElementById('happy-emoji');
+
+happyEmoji.addEventListener('click', function () {
+  const responses = ["You go!", "Your smile just cheered me up!", "YAY!"]
+  console.log(responses[0])
+})
