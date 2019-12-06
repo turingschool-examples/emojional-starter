5371356d27edbfb88932cdf60d7e1161e84a602e
Erik Raith <raithhtiar@gmail.com>
2019-12-06 10:04:51 -0700

    Add message functionality in for showResponse
    +show Response only uses one portion of the array

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //Define Variables needed for happyButton Functionality
-const happyButton = document.querySelector('.happy');
+const happyButton = document.querySelector(".happy");
 const messageElement = document.querySelector('.message');
+const happyMessage = ['You go!','Your smile just cheered me up!','YAY!'];
 
 function showResponse(){
   //Show Response
+
+  messageElement.innerHTML = happyMessage[1];
 }
 happyButton.addEventListener('click',showResponse);
