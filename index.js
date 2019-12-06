0deec72c94d4bff7a6947e09b2e4b9e3293e5421
kmbarnett <kmbarnett94@gmail.com>
2019-12-06 09:28:09 -0700

    Add inital vars and event listener for happy button

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var happy = document.querySelector('.happy')
+var silly = document.querySelector('.silly')
+var crying = document.querySelector('.crying')
+var message = document.querySelector('.message')
 
+
+
+
+
+
+happy.addEventListener('click', happy)
