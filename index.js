f466032e37867025c8b68865dcdcff05468c2fd6
Mandy Alford <mandyalford@gmail.com>
2019-12-06 09:43:24 -0700

    Added functionality for all three buttons to generate a random response

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+var happyBtn= document.querySelector('.happy');
+var sillyBtn= document.querySelector('.silly');
+var cryingBtn= document.querySelector('.crying');
+var messageSlot= document.querySelector('.message');
 
+var happyResponses= ['You go!', 'Your smile just cheered me up!', 'YAY!'];
+
+var sillyReponses= ['Interesting response', 'Sounds ... good?', 'Me too.'];
+
+var cryingResponses= ['Do you want to talk?', 'Keep your head up', 'We can cry together!', 'What can I do?']
+
+var index1= Math.floor(Math.random() * happyResponses.length);
+var index2= Math.floor(Math.random() * sillyReponses.length);
+var index3= Math.floor(Math.random() * cryingReponses.length);
+
+happyBtn.addEventListener('click', generateResponse);
+sillyBtn.addEventListener('click', generateResponse);
+cryingBtn.addEventListener('click', generateResponse);
+
+function generateResponse(event){
+  if (event.classList.contains('happy')){
+    messageSlot.innerText= happyResponses[index1];
+  } else if (event.classList.contains('silly')){
+    messageSlot.innerText= sillyResponses[index2];
+  } else if (event.classList.contains('.crying')){
+    messageSlot.innetText= cryingResponses[index3];
+  }
+}
