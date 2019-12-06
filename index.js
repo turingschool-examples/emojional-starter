b78f496356f80481b81d8ea0d9df2b2c941d97e6
Olivia Webster <oliviacweb@gmail.com>
2019-12-06 09:45:20 -0700

    Add functionality on happy face button

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+var happy = document.querySelector(".happy");
+var response = document.querySelector("h2");
+happy.addEventListener('click', happyResponse);
+function happyResponse() {
+  var happyResponse = `Well that's just peachy`;
+  response.innerText = happyResponse;
+}
