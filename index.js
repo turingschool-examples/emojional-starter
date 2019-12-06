236abc590f0071a081d8b62b6e04df846d93315d
Harry Borrelli <hborrelli@outlook.com>
2019-12-06 09:46:44 -0700

    Made changes without creating new branch. Commented out new code, will make new branch and uncomment changes.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 // Variables
 var happyFaceBtn = document.querySelector('.happy');
+// var sillyFaceBtn = document.querySelector('.silly');
+// var cryingFaceBtn = document.querySelector('.crying');
 var message = document.querySelector('.message');
 
 // Event Listeners
 happyFaceBtn.addEventListener('click', happyFace);
+sillyFaceBtn.addEventListener('click', sillyFace);
+// cryingFaceBtn.addEventListener('click', cryingFace);
 
 // Functions
 function happyFace() {
   var prompts = ['YAY!', 'You go!', 'Your smile just cheered me up.'];
 
-  message.innerHTML = prompts[randNum()];
+  message.innerHTML = prompts[randNum(3)];
 }
 
-function randNum() {
-  return Math.floor(Math.random() * Math.floor(3));
+// function sillyFace() {
+//   var prompts = ['Me too.', 'Sounds... good?', 'Ok!', 'Interesting response'];
+//
+//   message.innerHTML = prompts[randNum(4)];
+// }
+
+
+function randNum(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
