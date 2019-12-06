ebce1177d2d996ab17f292f1ca1c66bd937097c3
Taras Tarlov <ttarlov@gmail.com>
2019-12-06 11:57:13 -0700

    All i was able to get done

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ var happyBtn = document.querySelector('.happy');
 var sillyBtn = document.querySelector('.silly');
 var cryingBtn = document.querySelector('.crying');
 var message = document.querySelector('.message');
+var cryArray = ['All i can do is cry', 'Its a sad day', 'River of tears'];
+
 
 cryingBtn.addEventListener('click', startCrying);
 
+
+
 function startCrying() {
-  message.
-};
+  message.innerText = 'All i can do is cry';
+
+}
