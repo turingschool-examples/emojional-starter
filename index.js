87249aed6572a5d7dcddb3ae2d6910035a5aedc7
Harry Borrelli <hborrelli@outlook.com>
2019-12-06 10:37:41 -0700

    Complete functionality for Level Up challenge for each button. git status

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ happyFaceBtn.addEventListener('click', happyFace);
 sillyFaceBtn.addEventListener('click', sillyFace);
 cryingFaceBtn.addEventListener('click', cryingFace);
 
-
-
 // Functions
 function happyFace() {
   var prompts = ['YAY!', 'You go!', 'Your smile just cheered me up.'];
@@ -37,21 +35,39 @@ function happyFace() {
   console.log(`New last random number : ${lastRandNum}`);
 }
 
-
-
-
 function sillyFace() {
   var prompts = ['Me too.', 'Sounds... good?', 'Ok!', 'Interesting response'];
 
-  message.innerHTML = prompts[randNum(4)];
+  // Set Random number.
+  randNumber = randNum(4);
+
+  while(randNumber == lastRandNum) {
+    randNumber = randNum(4);
+  }
+
+  // Set diff random number to index of prompt
+  message.innerHTML = prompts[randNumber];
+
+  // Reassign the random number used to lastRandNum
+  lastRandNum = randNumber;
 }
 
 function cryingFace() {
   var prompts = ['What can I do?', 'Keep your head up.', 'Do you want to talk?', 'We can cry together!'];
 
-  message.innerHTML = prompts[randNum(4)];
-}
+  // Set Random number.
+  randNumber = randNum(4);
 
+  while(randNumber == lastRandNum) {
+    randNumber = randNum(4);
+  }
+
+  // Set diff random number to index of prompt
+  message.innerHTML = prompts[randNumber];
+
+  // Reassign the random number used to lastRandNum
+  lastRandNum = randNumber;
+}
 
 function randNum(max) {
   return Math.floor(Math.random() * Math.floor(max));
