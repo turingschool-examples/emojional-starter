d2505048e8008ac009f7cffe795eeae3b69e1c1d
Jonathan Tschida <jonathan.tschida@outlook.com>
2019-12-06 09:21:24 -0700

    Added basic functionality for happyButton

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ var message = document.querySelector('.message');
 var happyButton = document.querySelector('.happy');
 var sillyButton = document.querySelector('.silly');
 var cryingButton = document.querySelector('.crying');
+
+happyButton.addEventListener('click', displayHappyMessage);
+
+function displayHappyMessage() {
+  message.innerText = "Happy Message";
+}
