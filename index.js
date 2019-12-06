43d9e0b4d1da7195be82ac793995ca1817bb50f7
Lili Manrique <liliana.manrique1@gmail.com>
2019-12-06 09:27:50 -0700

    Add happy button functionality and makeRandomNumber function for nums between 0-2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+var happyMsgs = ['You go!', 'Your smile just cheered me up!', 'YAY!'];
 
+var happyBtn = document.querySelector('.happy');
+var msgDisplay = document.querySelector('.message')
+
+happyBtn.addEventListener('click', randomHappyMsg)
+
+function makeRandomNumber() {
+  return Math.floor(Math.random() * (2 + 1));
+}
+
+function randomHappyMsg() {
+  msgDisplay.innerText = happyMsgs[makeRandomNumber()];
+}
