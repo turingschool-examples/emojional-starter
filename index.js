2315484b0b5ff4b485398ae090816398d883919c
Harry Borrelli <hborrelli@outlook.com>
2019-12-06 10:26:44 -0700

    Progress towards generating a new random number each time.

--- index.js
+++ index.js
@@ -3,18 +3,40 @@ var happyFaceBtn = document.querySelector('.happy');
 var sillyFaceBtn = document.querySelector('.silly');
 var cryingFaceBtn = document.querySelector('.crying');
 var message = document.querySelector('.message');
+var lastRandNum = null;
+var randNumber = null;
 
 // Event Listeners
 happyFaceBtn.addEventListener('click', happyFace);
 sillyFaceBtn.addEventListener('click', sillyFace);
 cryingFaceBtn.addEventListener('click', cryingFace);
 
+
+
 // Functions
 function happyFace() {
   var prompts = ['YAY!', 'You go!', 'Your smile just cheered me up.'];
 
-  message.innerHTML = prompts[randNum(3)];
+  console.log(`last random number : ${lastRandNum}`);
+  // Set Random number.
+  randNumber = randNum(3);
+
+  console.log(`New random number : ${randNumber}`);
+
+  while(randNumber == lastRandNum) {
+    randNumber = randNum(max);
+  }
+  console.log(`Used random number : ${randNumber}`);
+  // Set diff random number to index of prompt
+  message.innerHTML = prompts[randNumber];
+
+  // Reassign the random number used to lastRandNum
+  lastRandNum = randNumber;
+  console.log(`New last random number : ${lastRandNum}`);
 }
+
+
+
 
 function sillyFace() {
   var prompts = ['Me too.', 'Sounds... good?', 'Ok!', 'Interesting response'];
