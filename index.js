ab276dc22f2d563ac8cd13f1341460db15418a5e
Kimala Cochran <kimalajoy@gmail.com>
2019-12-06 10:20:33 -0700

    Working on happy functionality

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var happyButton = document.querySelector("#happyButton");
+var response = document.querySelector("#message")
 
+happyButton.addEventListener("click", addHappyMessage);
+
+function addHappyMessage() {
+  var happyMessage = "Woo!";
+  message.innerText = happyMessage;
+}
+console.log("buttonclick", addHappyMessage);
