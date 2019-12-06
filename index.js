737c6d2251d032a53d48ba866a114dfd4aea7344
Mandy Alford <mandyalford@gmail.com>
2019-12-06 10:45:30 -0700

    create functionality for happy btn

--- index.js
+++ index.js
@@ -5,24 +5,34 @@ var messageSlot= document.querySelector('.message');
 
 var happyResponses= ['You go!', 'Your smile just cheered me up!', 'YAY!'];
 
-var sillyReponses= ['Interesting response', 'Sounds ... good?', 'Me too.'];
+var sillyResponses= ['Interesting response', 'Sounds ... good?', 'Me too.'];
 
-var cryingResponses= ['Do you want to talk?', 'Keep your head up', 'We can cry together!', 'What can I do?']
+var cryingResponses= ['Do you want to talk?', 'Keep your head up', 'We can cry together!', 'What can I do?'];
 
-var index1= Math.floor(Math.random() * happyResponses.length);
-var index2= Math.floor(Math.random() * sillyReponses.length);
-var index3= Math.floor(Math.random() * cryingReponses.length);
+// var index1= Math.floor(Math.random() * happyResponses.length);
+var index2= Math.floor(Math.random() * sillyResponses.length);
+var index3= Math.floor(Math.random() * cryingResponses.length);
 
-happyBtn.addEventListener('click', generateResponse);
-sillyBtn.addEventListener('click', generateResponse);
-cryingBtn.addEventListener('click', generateResponse);
+happyBtn.addEventListener('click', generateHappyResponse);
+// sillyBtn.addEventListener('click', generateResponse);
+// cryingBtn.addEventListener('click', generateResponse);
 
-function generateResponse(event){
-  if (event.classList.contains('happy')){
-    messageSlot.innerText= happyResponses[index1];
-  } else if (event.classList.contains('silly')){
-    messageSlot.innerText= sillyResponses[index2];
-  } else if (event.classList.contains('.crying')){
-    messageSlot.innetText= cryingResponses[index3];
-  }
+function generateHappyResponse(event){
+  var index1= Math.floor(Math.random() * happyResponses.length);
+  console.log('clicked!');
+  messageSlot.innerText= happyResponses[index1];
 }
+
+// function resetBtn() {
+//   document.getElementById('button').reset();
+// }
+
+// function generateResponse(event){
+//   if (target.classList.contains('happy')){
+//     messageSlot.innerText= happyResponses[index1];
+//   } else if (event.classList.contains('silly')){
+//     messageSlot.innerText= sillyResponses[index2];
+//   } else if (event.classList.contains('.crying')){
+//     messageSlot.innetText= cryingResponses[index3];
+//   }
+// }
