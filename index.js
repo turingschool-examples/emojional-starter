450313b841be27c412598f96ad50e72b823853cd
Madison Randle <madisonrandle25@gmail.com>
2019-12-06 10:06:03 -0700

    Add event listeners to each emoji

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var message = document.querySelector(".message");
 var happyEmoji = document.querySelector(".happy");
 var sillyEmoji = document.querySelector(".silly");
 var cryingEmoji = document.querySelector(".crying");
+
+happyEmoji.addEventListener("click", showHappyResponse);
+sillyEmoji.addEventListener("click", showSillyResponse);
+cryingEmoji.addEventListener("click", showCryingResponse);
