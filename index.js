925d8dfd00bf8e78f8a325187ecf2084cf444234
Madison Randle <madisonrandle25@gmail.com>
2019-12-06 10:07:39 -0700

    Create functions

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ var cryingEmoji = document.querySelector(".crying");
 happyEmoji.addEventListener("click", showHappyResponse);
 sillyEmoji.addEventListener("click", showSillyResponse);
 cryingEmoji.addEventListener("click", showCryingResponse);
+
+function showHappyResponse() {
+  var responseMsg = `
+
+     `;
+      cardContainerParent.insertAdjacentHTML("afterbegin", responseMsg);
+}
+
+function showSillyResponse() {
+  var responseMsg = `
+
+     `;
+      cardContainerParent.insertAdjacentHTML("afterbegin", responseMsg);
+}
+
+function showCryingResponse() {
+  var responseMsg = `
+
+     `;
+      cardContainerParent.insertAdjacentHTML("afterbegin", responseMsg);
+}
