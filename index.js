d1b8879f59bc944ab8bb66e92644b9e53057d1af
Harry Borrelli <hborrelli@outlook.com>
2019-12-06 09:36:56 -0700

    Declare global variables. Build happyFace function with event listener. Complete function for first button.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+// Variables
+var happyFaceBtn = document.querySelector('.happy');
+var message = document.querySelector('.message');
 
+// Event Listeners
+happyFaceBtn.addEventListener('click', happyFace);
+
+// Functions
+function happyFace() {
+  var prompts = ['YAY!', 'You go!', 'Your smile just cheered me up.'];
+
+  message.innerHTML = prompts[randNum()];
+}
+
+function randNum() {
+  return Math.floor(Math.random() * Math.floor(3));
+}
