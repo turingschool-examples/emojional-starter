099161c0260fb8695d2bada82bbffd35c3e8e4a8
Lili Manrique <liliana.manrique1@gmail.com>
2019-12-06 09:37:37 -0700

    Declare functions and add event listener to happy button

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 var happyBtn = document.querySelector('.happy');
 var happyMsgs = ['You go!', 'Your smile just cheered me up!', 'YAY!'];
 var msgDisplay = document.querySelector('.message')
+
+happyBtn.addEventListener('click', randomHappyMsg)
+
+function makeRandomNumber() {
+  return Math.floor(Math.random() * (2 + 1));
+}
+
+function randomHappyMsg() {
+  msgDisplay.innerText = happyMsgs[makeRandomNumber()];
+}
