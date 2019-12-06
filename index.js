4d6c221a3a488624a21031d02a00176bf278dd07
Megan Venetianer <megan.venetianer@gmail.com>
2019-12-06 11:51:58 -0700

    Completed iteration 2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var sillyButton = document.querySelector('.silly');
 var sillyMessage = ['Interesting response', 'Sounds...good?', 'Me too.'];
 var cryingButton = document.querySelector('.crying');
 var cryingMessage = ['Do you want to talk?', 'Keep your head up', 'We can cry together!', 'What can I do?'];
+var currentMessage = 0;
 
 happyButton.addEventListener('click', addHappyResponse);
 sillyButton.addEventListener('click', addSillyResponse);
@@ -11,17 +12,30 @@ cryingButton.addEventListener('click', addCryingResponse);
 
 function addHappyResponse() {
   var randomMessage = Math.floor(Math.random() * (happyMessage.length));
+  if(randomMessage == currentMessage) {
+    addHappyResponse();
+  } else {
+    currentMessage = randomMessage;
   document.querySelector('.message').innerHTML = happyMessage[randomMessage];
+  }
 }
 
 function addSillyResponse() {
   var randomMessage = Math.floor(Math.random() * (sillyMessage.length));
+  if(randomMessage == currentMessage) {
+    addSillyResponse();
+  } else {
+    currentMessage = randomMessage;
   document.querySelector('.message').innerHTML = sillyMessage[randomMessage];
+  }
 }
 
 function addCryingResponse() {
   var randomMessage = Math.floor(Math.random() * (cryingMessage.length));
+  if(randomMessage == currentMessage) {
+    addCryingResponse();
+  } else {
+    currentMessage = randomMessage;
   document.querySelector('.message').innerHTML = cryingMessage[randomMessage];
+  }
 }
-
-// fixing cursor
