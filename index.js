87acfc0180509be3c6a3d6543b8226476204f1f0
kmbarnett <kmbarnett94@gmail.com>
2019-12-06 10:02:10 -0700

    Fix bug no return on random saying generator

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var message = document.querySelector('.message')
 
 
 function randomSaying() {
-  Math.ceil(Math.random() * 3);
+  return Math.ceil(Math.random() * 3);
 }
-
+console.log(randomSaying());
 function handleHappy(i) {
   i = randomSaying()
+
   if (i === 1) {
     message.innerText = 'You go!'
   }else if (i === 2) {
@@ -20,32 +21,32 @@ function handleHappy(i) {
 
 }
 
-function handleSilly(i) {
-  i = randomSaying()
-  if (i === 1) {
-    message.innerText = 'Interesting response'
-  }else if (i === 2) {
-    message.innerText = 'Sounds ... good?'
-  }else if (i === 3) {
-    message.innerText = 'Me too.'
-  }
-
-}
-
-function handleCrying(i) {
-  i = randomSaying()
-  if (i === 1) {
-    message.innerText = 'Do you want to talk?'
-  }else if (i === 2) {
-    message.innerText = 'Keep your head up'
-  }else if (i === 3) {
-    message.innerText = 'We can cry together!'
-  }
-
-}
+// function handleSilly(i) {
+//   i = randomSaying()
+//   if (i === 1) {
+//     message.innerText = 'Interesting response'
+//   }else if (i === 2) {
+//     message.innerText = 'Sounds ... good?'
+//   }else if (i === 3) {
+//     message.innerText = 'Me too.'
+//   }
+//
+// }
+//
+// function handleCrying(i) {
+//   i = randomSaying()
+//   if (i === 1) {
+//     message.innerText = 'Do you want to talk?'
+//   }else if (i === 2) {
+//     message.innerText = 'Keep your head up'
+//   }else if (i === 3) {
+//     message.innerText = 'We can cry together!'
+//   }
+//
+// }
 
 
 
 happy.addEventListener('click', handleHappy)
-silly.addEventListener('click', handleSilly)
-crying.addEventListener('click', handleCrying)
+// silly.addEventListener('click', handleSilly)
+// crying.addEventListener('click', handleCrying)
