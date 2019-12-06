7ad0e834427cd9d92f7ec2bc62e6cdcadf9fa8ce
Erik Raith <raithhtiar@gmail.com>
2019-12-06 09:53:37 -0700

    Add Framework for variables needed in showMessage Function.
    +Function is not complete, only framework and eventlister

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+//Define Variables needed for happyButton Functionality
+const happyButton = document.querySelector('.happy');
+const messageElement = document.querySelector('.message');
 
+function showResponse(){
+  //Show Response
+}
+happyButton.addEventListener('click',showResponse);
