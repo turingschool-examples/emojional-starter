55d6a1e889000fcae1d796316d9e8a2a26595d92
Mandy Alford <mandyalford@gmail.com>
2019-12-06 11:14:24 -0700

    Add happy and silly button func

--- index.js
+++ index.js
@@ -10,18 +10,19 @@ var sillyResponses= ['Interesting response', 'Sounds ... good?', 'Me too.'];
 var cryingResponses= ['Do you want to talk?', 'Keep your head up', 'We can cry together!', 'What can I do?']
 
 
-var index2= Math.floor(Math.random() * sillyResponses.length);
+
 var index3= Math.floor(Math.random() * cryingResponses.length);
 
 happyBtn.addEventListener('click', generateHappyResponse);
-// sillyBtn.addEventListener('click', generateResponse);
+sillyBtn.addEventListener('click', generateSillyResponse);
 // cryingBtn.addEventListener('click', generateResponse);
 
 function generateHappyResponse(event){
-  var index1= Math.floor(Math.random() * happyResponses.length);
+  var index= Math.floor(Math.random() * happyResponses.length);
+    messageSlot.innerText= happyResponses[index];
+}
 
-    messageSlot.innerText= happyResponses[index1];
-  // } else if (event.classList.contains('silly')){
-  //   messageSlot.innerText= sillyResponses[index2];
-
+function generateSillyResponse(event){
+  var index= Math.floor(Math.random() * sillyResponses.length);
+    messageSlot.innerText= sillyResponses[index];
 }
