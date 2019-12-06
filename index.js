d7f24b19761d14b5513feecd89d49dd8450fc4dc
Charlie Bandstra <bandstracharlie@gmail.com>
2019-12-06 12:01:32 -0700

    Changed button to display message after click

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var happyButton = document.querySelector('.happy');
+var message = document.querySelector('.message');
 
+
+happyButton.addEventListener('click', clickMessage1);
+  function clickMessage1() {
+  message.innerText = "You go!";
+}
+  // I am really struggling with how to even start on showing up multiple messages.
+  // I feel like there should be an array but I am out of time. Ill start javascript
+  // fromsquare one this weekend and try to get a better understandig of what is
+  // happenening.
