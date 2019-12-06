6aef95ccf1edfa3832468e6ce75f84f4cd54a3a6
Edward Stoner <edwardlstoner@gmail.com>
2019-12-06 09:41:44 -0700

    Add response messages

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var happyEmoji = document.querySelector('.happy');
 var sillyEmoji = document.querySelector('.silly');
 var cryingEmoji = document.querySelector('.crying');
+var happyResponses = ["You go!", "Your smile just cheered me up!", "YAY!"];
+var sillyResponses = ["Interesting response", "Sounds ... good?", "Me too."];
+var cryingResponses = ["Do you want to talk?", "Keep your head up", "We can cry together!","What can I do?"]
