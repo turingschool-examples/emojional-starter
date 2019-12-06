edafcf849d7648a2b1fbd8a92174689221b267f8
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-06 09:41:12 -0700

    Add functionality to display an array of messages for click on happy btn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ var happyBtn = document.querySelector('.happy');
 var message = document.querySelector('.message');
 
 happyBth.addEventListener('click', displayHappyMessage);
+
+
+function displayHappyMessage() {
+  var happyMessages = ['Go, you!', 'Your smile just cheered me up!', 'YAY!', 'It\'s a great day to be happy!', 'You\'re walking on sunshine over there!', 'Doing the happy dance!', 'Happy, happy, happy!'];
+  randomIndex = Math.floor(Math.random() * happyMessages.length);
+
+  message.innerText = happyMessages[randomIndex];
+}
