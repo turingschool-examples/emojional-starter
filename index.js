60a118e194bde904b0fe3421cd89f885f0ae4bd3
Jonathan Tschida <jonathan.tschida@outlook.com>
2019-12-06 09:18:46 -0700

    Added variables for selecting relevant objects

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+var message = document.querySelector('.message');
+var happyButton = document.querySelector('.happy');
+var sillyButton = document.querySelector('.silly');
+var cryingButton = document.querySelector('.crying');
