edf99eb54c5369df66060f8dfdb35ad6d4867028
Kimala Cochran <kimalajoy@gmail.com>
2019-12-06 11:21:28 -0700

    Completed Silly button functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// Happy Button
+// // Happy Button
 var happyButton = document.querySelector("#happyButton");
-var response = document.querySelector("#message")
+var happyMessage = document.querySelector("#message")
 
 happyButton.addEventListener("click", addHappyMessage);
 
@@ -10,18 +10,13 @@ function addHappyMessage() {
   message.innerText = randomHappy;
 }
 // Silly Button
-var sillyButton = document.querySelector("#happyButton");
-var response = document.querySelector("#message")
+var sillyButton = document.querySelector("#sillyButton");
+var sillyMessage = document.querySelector("#message")
 
-happyButton.addEventListener("click", addHappyMessage);
+sillyButton.addEventListener("click", addSillyMessage);
 
-function addHappyMessage() {
-  var happyMessage = ["Woo!", "Yay!", "You go!"];
-  var randomHappy = happyMessage[Math.floor(Math.random()*happyMessage.length)];
-  message.innerText = randomHappy;
-
-// console.log("buttonclick", randomHappy);
-
-
-// let randomHappy = Math.floor(Math.random() * happyMessage.length);
-// return happyMessage = ["Woo!", "You go!", "Yay!"];
+function addSillyMessage() {
+  var sillyMessage = ["Same.", "If you say so!", "Great googly moogly!"];
+  var randomSilly = sillyMessage[Math.floor(Math.random()*sillyMessage.length)];
+  message.innerText = randomSilly;
+}
