29567e265ed114515e1cfbbddada08cda71135e3
Lane Hunter <lanehunter.96@gmail.com>
2019-12-06 10:17:48 -0700

    Add dom element vars & event listeners

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var happyButton = document.querySelector(".happy");
+var sillyButton = document.querySelector(".silly");
+var cryingButton = document.querySelector(".crying");
+var message = document.querySelector(".message")
+var happyMessages = ["You go!", "Your smile just cheered me up!", "YAY!"];
+var sillyMessages = ["Interesting response...", "Sounds ... good?", "Me too."];
+var cryingMessages = ["Do you want to talk?", "Keep your head up", "We can cry together!", "What can I do?"];
 
+happyButton.addEventListener("click", showHappyMsg);
+sillyButton.addEventListener("click", showSillyMsg);
+cryingButton.addEventListener("click", showCryingMsg);
