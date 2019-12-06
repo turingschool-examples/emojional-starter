909bac8ba7cdaf5adf11f925412e4a5873d4fb4d
Taras Tarlov <ttarlov@gmail.com>
2019-12-06 11:00:40 -0700

    Adds button variables and one event listener to cry button

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+var happyBtn = document.querySelector('.happy');
+var sillyBtn = document.querySelector('.silly');
+var cryingBtn = document.querySelector('.crying');
+var message = document.querySelector('.message');
+
+cryingBtn.addEventListener('click', startCrying);
+
+function startCrying() {
+  message.
+};
