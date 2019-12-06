fbbb6c57b680ebfd8084e0cd31522c9ff9ca09f0
kmbarnett <kmbarnett94@gmail.com>
2019-12-06 11:08:10 -0700

    Remove all console.log Add ; to be a nice coder

--- index.js
+++ index.js
@@ -1,64 +1,62 @@
-var happy = document.querySelector('.happy')
-var silly = document.querySelector('.silly')
-var crying = document.querySelector('.crying')
-var message = document.querySelector('.message')
-var nextNum
-var repeat
+var happy = document.querySelector('.happy');
+var silly = document.querySelector('.silly');
+var crying = document.querySelector('.crying');
+var message = document.querySelector('.message');
+var nextNum;
+var repeat;
 
 function randomSaying() {
   return Math.ceil(Math.random() * 3);
 }
 
 function blockRepeat() {
-  var repeat = randomSaying()
+  var repeat = randomSaying();
   if (repeat === nextNum) {
-    blockRepeat()
+    blockRepeat();
   }else {
-    nextNum = repeat
-    console.log(nextNum);
+    nextNum = repeat;
   }
-  console.log(nextNum);
-  return nextNum
+  return nextNum;
 }
 
 function handleHappy(i) {
-  i = blockRepeat()
+  i = blockRepeat();
   if (i === 1) {
-    message.innerText = 'You go!'
+    message.innerText = 'You go!';
   }else if (i === 2) {
-    message.innerText = 'Your smile just cheered me up!'
+    message.innerText = 'Your smile just cheered me up!';
   }else if (i === 3) {
-    message.innerText = 'YAY!'
+    message.innerText = 'YAY!';
   }
 
 }
 
 function handleSilly(i) {
-  i = blockRepeat()
+  i = blockRepeat();
   if (i === 1) {
-    message.innerText = 'Interesting response'
+    message.innerText = 'Interesting response';
   }else if (i === 2) {
-    message.innerText = 'Sounds ... good?'
+    message.innerText = 'Sounds ... good?';
   }else if (i === 3) {
-    message.innerText = 'Me too.'
+    message.innerText = 'Me too.';
   }
 
 }
 
 function handleCrying(i) {
-  i = blockRepeat()
+  i = blockRepeat();
   if (i === 1) {
-    message.innerText = 'Do you want to talk?'
+    message.innerText = 'Do you want to talk?';
   }else if (i === 2) {
-    message.innerText = 'Keep your head up'
+    message.innerText = 'Keep your head up';
   }else if (i === 3) {
-    message.innerText = 'We can cry together!'
+    message.innerText = 'We can cry together!';
   }
 
 }
 
 
 
-happy.addEventListener('click', handleHappy)
-silly.addEventListener('click', handleSilly)
-crying.addEventListener('click', handleCrying)
+happy.addEventListener('click', handleHappy);
+silly.addEventListener('click', handleSilly);
+crying.addEventListener('click', handleCrying);
