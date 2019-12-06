9eda8a9ab7708cf2a59a9fddaba13dd2f50083c5
Jordan Whitten <jordanwa1947@gmail.com>
2019-12-06 09:35:07 -0700

    Generate random number from 0 to 2 and use as index to select a message

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const emojiMessage = document.getElementById('emoji-message');
 
 happyEmoji.addEventListener('click', function () {
   const responses = ["You go!", "Your smile just cheered me up!", "YAY!"];
-  emojiMessage.innerText = responses[0];
+  let randIndex = Math.floor(Math.random() * 3);
+  emojiMessage.innerText = responses[randIndex];
 })
