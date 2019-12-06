d62ff33b47741bf236d2a032e1bf8f00cd74366b
Dan Reardon <dreardon1021@gmail.com>
2019-12-06 09:39:09 -0700

    Create happy button functionallity

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var happyButton = document.querySelector('.happy');
+var message = document.querySelector('.message');
 
+
+
+
+happyButton.addEventListener ('click', addHappyMessage);
+
+function addHappyMessage (){
+  console.log ('addHappyMessage')
+  var random = Math.floor(Math.random() * 3);
+  if (random === 0){
+      message.innerText = 'You go!';
+    } else if (random === 1) {
+      message.innerText = 'Your smile just cheered me up!';
+    } else if (random === 2){
+      message.innerText = 'YAY!';;
+    } else {
+      message.innerText = 'ERROR!!';
+  };
+};
