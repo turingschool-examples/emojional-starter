97799e1f8438f2bc70dffcf071b34a5706d6c9ff
Lili Manrique <liliana.manrique1@gmail.com>
2019-12-06 09:52:59 -0700

    Add variables and event listeners for other emojis, add parameters to functions to make them customizable

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var happyBtn = document.querySelector('.happy');
 var happyMsgs = ['You go!', 'Your smile just cheered me up!', 'YAY!'];
-var msgDisplay = document.querySelector('.message')
+var msgDisplay = document.querySelector('.message');
+var sillyMsgs = ['Interesting response', 'Sounds ... good?', 'Me too.'];
+var sadMsgs = ['Do you want to talk?', 'Keep your head up', 'We can cry together!',
+'What can I do?'];
+var sadBtn = document.querySelector('.crying');
+var sillyBtn = document.querySelector('.silly');
 
-happyBtn.addEventListener('click', randomHappyMsg)
+happyBtn.addEventListener('click', () => {randomMsg(happyMsgs, 2, 0)});
+sillyBtn.addEventListener('click', () => {randomMsg(sillyMsgs, 2, 0)});
+sadBtn.addEventListener('click', () => {randomMsg(sadMsgs, 3, 0)});
 
-function makeRandomNumber() {
-  return Math.floor(Math.random() * (2 + 1));
+function makeRandomNumber(max, min) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function randomHappyMsg() {
-  msgDisplay.innerText = happyMsgs[makeRandomNumber()];
+function randomMsg(msgArray, max, min) {
+  msgDisplay.innerText = msgArray[makeRandomNumber(max, min)];
 }
