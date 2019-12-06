cb0089464466440ab57b78c8763bb85ab4f2762d
Erik Raith <raithhtiar@gmail.com>
2019-12-06 10:15:07 -0700

    Add variables and array of messages

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //Define Variables needed for happyButton Functionality
 const happyButton = document.querySelector(".happy");
+const sillyButton = document.querySelector(".silly");
+const cryingButton = document.querySelector(".crying");
 const messageElement = document.querySelector('.message');
 const happyMessage = ['You go!','Your smile just cheered me up!','YAY!'];
+const sillyMessage = ['Interesting response','Sounds... good?','Me too.'];
+const cryingMessage = ['Do you want to talk?','Keep your head up','We can cry together','What can I do?'];
 
 function showResponse(){
   //Show Response
   var i = Math.floor(Math.random()*happyMessage.length);
-  messageElement.innerHTML = happyMessage[1];
+  messageElement.innerHTML = happyMessage[i];
 }
 happyButton.addEventListener('click',showResponse);
