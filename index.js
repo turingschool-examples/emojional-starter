ff1af4c21b087efe43549bfffd199fccc91d67cf
Erin Campbell <iheartmnight@gmail.com>
2019-12-06 11:24:53 -0700

    git problems

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+var happyButton = document.querySelector("#happy");
+var quotes = ["You go!", "It's a great day", "It's good"]
 
+happyButton.addEventListener("click", showHappyMessages);
+console.log('cant get it');
+
+// click on button and get message
+function showHappyMessages() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById("displayed-message").innerHTML = quotes[randomNumber];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
