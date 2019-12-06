52bf82cc1bf620679cb27bfb69ce9137e0c1fba7
Madison Randle <madisonrandle25@gmail.com>
2019-12-06 10:05:23 -0700

    Declare global variables

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+var message = document.querySelector(".message");
+var happyEmoji = document.querySelector(".happy");
+var sillyEmoji = document.querySelector(".silly");
+var cryingEmoji = document.querySelector(".crying");
