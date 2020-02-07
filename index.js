84d1f868d9c51ccecff1e863d8b4cfe2b9c0dea5
Ruby Rinken <rubyrinken@gmail.com>
2020-02-07 09:57:16 -0700

    Add basic functionality

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+var message = document.querySelector(".message");
+var happyButton = document.querySelector(".happy");
+var happyResponses = [
+  "You go!",
+  "Your smile just cheered me up!",
+  "YAY!"
+];
+
+happyButton.addEventListener("click", messageHappy);
+
+function messageHappy() {
+  message.innerText = happyResponses[0];
+}
